2c5572acb78d721435c6793eeede8f7548e4e8e9
Sagar Shinde <asagarshinde@gmail.com>
2018-08-25 15:34:28 +0530

    Update argument_parser_template.py

--- argument_parser_template.py
+++ argument_parser_template.py
@@ -3,7 +3,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("-z","--zabbix")
 parser.add_argument("-v","--verbose",action="store_true")
 parser.add_argument("-lb","--load-balancer",choices=[1,2,3,4], type=int,nargs='*',
-                   help="1. BidSwitch-SSP-LB \n 2. Google-Bidder-US \n 3. smaato-LB \n 4. XML-LB")
+                   help="1. First choice \n 2. Second Choice \n 3. Third choice \n 4. Fourth choice")
 args = parser.parse_args()
 print args
 
